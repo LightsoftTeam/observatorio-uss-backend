efc9e0a67a3f774ff0640c889bf07e2ddf393d18
josemr22 <josemr.99@outlook.com>
2024-08-23 15:24:38 -0500

    fix user field in comments response

---
+++ src/training/helpers/validate-participant-row.helper.ts
@@ -0,0 +1,43 @@
+import { ParticipantRow, MigrationDocumentType, MigrationEmploymentType } from '../types/training-migration.types';
+
+export function validateParticipantRow(participantRow: ParticipantRow) {
+    const {
+        asistencia,
+        email,
+        escuela,
+        nombre,
+        // roles,
+        "numero de documento": documentNumber,
+        "tipo de documento": documentType,
+        "tipo de empleo": employmentType,
+    } = participantRow;
+
+    if (!Object.keys(Boolean).includes(asistencia)) {
+        throw new Error('El campo "asistencia" tiene un valor inválido');
+    }
+    if (!email) {
+        throw new Error('El campo "email" es requerido');
+    }
+    if (!escuela) {
+        throw new Error('El campo "escuela" es requerido');
+    }
+    if (!nombre) {
+        throw new Error('El campo "nombre" es requerido');
+    }
+    if (!documentNumber) {
+        throw new Error('El campo "numero de documento" es requerido');
+    }
+    if (!documentType) {
+        throw new Error('El campo "tipo de documento" es requerido');
+    }
+    if(!Object.values(MigrationDocumentType).includes(documentType as MigrationDocumentType)){
+        throw new Error('El campo "tipo de documento" tiene un valor inválido');
+    }
+    if (!employmentType) {
+        throw new Error('El campo "tipo de empleo" es requerido');
+    }
+    if(!Object.values(MigrationEmploymentType).includes(employmentType as MigrationEmploymentType)){
+        throw new Error('El campo "tipo de empleo" tiene un valor inválido');
+    }
+    return participantRow;
+}
--- src/training/types/training-migration.types.ts
+++ src/training/types/training-migration.types.ts
@@ -5,7 +5,7 @@ export interface TrainingRow{
     tipo: string;
     "descripcion": string;
     "organizador": string;
-    "modalidad": string;
+    "modalidad": MigrationTrainingModality;
     "capacidad": string;
     "competencia": string;
     "fondo de certificado": string;
@@ -16,13 +16,45 @@ export interface TrainingRow{
     "hasta": string;
 }
 
+export enum MigrationTrainingModality {
+    PRESENCIAL = 'PRESENCIAL',
+    VIRTUAL = 'VIRTUAL',
+    SEMIPRESENCIAL = 'SEMIPRESENCIAL',
+}
+
+export enum MigrationTrainingType {
+    EXTRA = 'EXTRA',
+    PROGRAMADO = 'PROGRAMADO',
+}
+
+export enum BooleanResponse{
+    SI = 'SI',
+    NO = 'NO',
+}
+
+export enum MigrationTrainingRole {
+    ASISTENTE = 'ASISTENTE',
+    ORGANIZADOR = 'ORGANIZADOR',
+    PONENTE = 'PONENTE',
+}
+
+export enum MigrationEmploymentType {
+    FULLTIME = 'PARTTIME',
+    PARTTIME = 'FULLTIME',
+}
+
+export enum MigrationDocumentType {
+    DNI = 'DNI',
+    CE = 'CE',
+    PASAPORTE = 'PASAPORTE',
+}
 export interface ParticipantRow{
     nombre: string;
     email: string;
-    "tipo de documento": string;
+    "tipo de documento": MigrationDocumentType;
     "numero de documento": string;
     "escuela": string;
-    'tipo de empleo': string;
-    asistencia: string;
+    'tipo de empleo': MigrationEmploymentType;
+    asistencia: BooleanResponse;
     roles: string;
 }
